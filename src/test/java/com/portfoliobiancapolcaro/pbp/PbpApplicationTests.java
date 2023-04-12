2ba1be4b57844eb8209d39a6838e293ebf449c87
Bianca <biann026@gmail.com>
2023-04-12 16:15:31 -0300

    Start
    Agregue cositas con Spring Initializr

---
+++ src/test/java/com/portfoliobiancapolcaro/pbp/PbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliobiancapolcaro.pbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
